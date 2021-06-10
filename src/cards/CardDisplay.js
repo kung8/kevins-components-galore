4b2e5959b9ab10206ba7e61fe6ea1ad2e40ad38d
Kevin Ung <ung.kevin78@gmail.com>
2021-06-09 22:37:06 -0600

    Initialize Cards Component

---
+++ src/cards/CardDisplay.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Card from './CreatingCard';
+import cardsArr from './Cards';
+import clonedeep from 'lodash.clonedeep';
+
+function CardsDisplay() {
+    const [cards, updateCards] = useState(cardsArr);
+
+    const shuffle = async () => {
+        const copy = clonedeep(cards);
+        for (var i = copy.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = copy[i];
+            copy[i] = copy[j];
+            copy[j] = temp;
+        }
+        updateCards(copy);
+    }
+
+    const mappedCards = cards.map(card => {
+        card.show = true;
+        card.class = null;
+        return (
+            <Card key={card.card_id} card={card} />
+        )
+    })
+
+    return (
+        <div className="card-display-container">
+            <div className="card-display-holder">
+                <button className="shuffle-btn" onClick={shuffle}>Shuffle</button>
+                <div className="flex-wrap max-w-90-percent card-list">
+                    {mappedCards}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardsDisplay;
